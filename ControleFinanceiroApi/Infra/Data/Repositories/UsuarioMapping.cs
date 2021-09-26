1fe226c2eb94bfe98918523b114bcc8b734dfcd0
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-09-26 17:22:04 -0300

    add UsuarioMapping.cs

---
+++ ControleFinanceiroApi/Infra/Data/Repositories/UsuarioMapping.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Linq;
+using ControleFinanceiroApi.Business.Entities;
+using ControleFinanceiroApi.Business.IRepositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleFinanceiroApi.Infra.Data.Repositories
+{
+    public class UsuarioMapping : IUsuarioRepository
+    {
+        private readonly ControleFinanceiroApiDbContext _context;
+
+        public UsuarioMapping(ControleFinanceiroApiDbContext context)
+        {
+            _context = context;
+        }
+
+        public void adicionaUsuario(Usuario usuario)
+        {
+            _context.Add(usuario);
+        }
+
+        public void commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public List<Usuario> obterUsuarios(int UsuarioID)
+        {
+            return _context.Usuarios.Include(i => i.UsuarioID).Where(w => w.UsuarioID == UsuarioID).ToList();
+        }
+    }
+}
