a9b1bc4d7758429732112de4f10857ce317e8216
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-09-26 16:54:40 -0300

    add ContaPagarViewModelOutput.cs

---
+++ ControleFinanceiroApi/Models/ContaPagar/ContaPagarViewModelOutput.cs
@@ -0,0 +1,9 @@
+namespace ControleFinanceiroApi.Models.ContaPagar
+{
+    public class ContaPagarViewModelOutput
+    {
+        public string Nome { get; set; }
+        public string DataPagamento { get; set; }
+        public double Valor { get; set; }
+    }
+}
