3b12cb470ba36ce36b80645ff78f64e5022d4d9d
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-09-26 16:59:22 -0300

    corrigindo eroo digitação.

--- ControleFinanceiroApi/Models/ContaPagar/ContaPagarViewModelInput.cs
+++ ControleFinanceiroApi/Models/ContaPagar/ContaPagarViewModelInput.cs
@@ -6,9 +6,9 @@ namespace ControleFinanceiroApi.Models.ContaPagar
     {
         [Required(ErrorMessage = "O campo Nome e obrigatorio. ")]
         public string Nome { get; set; }
-        [Required(ErrorMessage = "O campo Nome e obrigatorio. ")]
+        [Required(ErrorMessage = "O campo Data de Pagamento e obrigatorio. ")]
         public string DataPagamento { get; set; }
-        [Required(ErrorMessage = "O campo Nome e obrigatorio. ")]
+        [Required(ErrorMessage = "O campo Valor e obrigatorio. ")]
         public double Valor { get; set; }
     }
 }
