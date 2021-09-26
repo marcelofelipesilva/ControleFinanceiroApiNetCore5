5adf9bf0ac05fd7222371aa3c10ab79f015d48ac
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-09-26 17:03:04 -0300

    add UsuarioViewModelOutput.cs

---
+++ ControleFinanceiroApi/Models/Usuario/UsuarioViewModelOutput.cs
@@ -0,0 +1,9 @@
+namespace ControleFinanceiroApi.Models.Usuario
+{
+    public class UsuarioViewModelOutput
+    {
+        public string Nome { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+    }
+}
