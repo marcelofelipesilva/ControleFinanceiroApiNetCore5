20a91db0e95cabec27311eadc0d7df0b574b14bd
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-09-26 16:58:36 -0300

    add ReceitaViewModelOutput.cs

---
+++ ControleFinanceiroApi/Models/Receita/ReceitaViewModelOutput.cs
@@ -0,0 +1,11 @@
+namespace ControleFinanceiroApi.Models.Receita
+{
+    public class ReceitaViewModelOutput
+    { 
+        public string Nome { get; set; }
+        public string DataRecebimento { get; set; }
+        public double Valor { get; set; }   
+    }
+        
+    
+}
