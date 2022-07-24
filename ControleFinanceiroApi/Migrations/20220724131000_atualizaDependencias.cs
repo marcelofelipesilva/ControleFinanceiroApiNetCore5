0aae411cbd70a966f6c96e3f761b4b2d3bbc039c
marcelofelipesilva <marcelofelipesilva@outlook.com>
2022-07-24 10:12:42 -0300

    atualizando migrations.

---
+++ ControleFinanceiroApi/Migrations/20220724131000_atualizaDependencias.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ControleFinanceiroApi.Migrations
+{
+    public partial class atualizaDependencias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
